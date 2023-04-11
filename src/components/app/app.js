f01cd833a10a322bfcf146679fe0c072b905e2bc
Aleksandr Zautsev <alexandr.falkone@yandex.ru>
2023-04-11 21:25:35 +0300

    add dynamic com of employee bonuses and promotions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,15 +6,21 @@ import EmployersAddForms from '../employersAddForm/employersAddForm';
 import './app.css';
 
 function App() {
+  const data = [
+    { name: 'Jhon C.', salary: 850, increase: false, id: 1 },
+    { name: 'Dima S.', salary: 1000, increase: true, id: 2 },
+    { name: 'Alex Z.', salary: 1250, increase: false, id: 3 },
+    { name: 'Mary L.', salary: 1450, increase: false, id: 4 },
+  ];
+
   return (
     <div className="app">
       <Info />
-
       <div className="search-panel">
         <SearchPanel />
         <Filter />
       </div>
-      <EmployersList />
+      <EmployersList data={data} />
       <EmployersAddForms />
     </div>
   );
--- src/components/employersList/employersList.js
+++ src/components/employersList/employersList.js
@@ -1,14 +1,13 @@
 import EmployersListItem from '../employersListItem/employersListItem';
 import './employersList.css';
 
-function EmployersList() {
-  return (
-    <ul className="app-list list-group">
-      <EmployersListItem />
-      <EmployersListItem />
-      <EmployersListItem />
-    </ul>
-  );
+function EmployersList({ data }) {
+  const elements = data.map((item) => {
+    const { id, ...itemProps } = item;
+    return <EmployersListItem key={id} {...itemProps} />;
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
 }
 
 export default EmployersList;
