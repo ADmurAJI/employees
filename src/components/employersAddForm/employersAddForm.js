cc62f88bd3be6f41d3e5a1fcedfbd4b59aa734e0
Aleksandr Zautsev <alexandr.falkone@yandex.ru>
2023-04-13 13:41:17 +0300

    added employee addition

--- src/components/employersAddForm/employersAddForm.js
+++ src/components/employersAddForm/employersAddForm.js
@@ -1,26 +1,51 @@
+import { Component } from 'react';
 import './employersAddForm.css';
 
-function EmployersAddForms() {
-  return (
-    <div className="app-add-form">
-      <h3>Добавить нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $"
-        />
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
+class EmployersAddForms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: '',
+    };
+  }
+
+  onValueChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>Добавить нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Имя сотрудника"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в $"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange}
+          />
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default EmployersAddForms;
